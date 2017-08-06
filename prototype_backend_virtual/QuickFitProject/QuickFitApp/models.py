ca6bf9dcc31f511239dd684cb84a04b0442da978
chrisbrickey <cbrickey1@gmail.com>
2017-08-06 03:43:15 -0700

    add Exercise model and migrate local database

--- prototype_backend_virtual/QuickFitProject/QuickFitApp/models.py
+++ prototype_backend_virtual/QuickFitProject/QuickFitApp/models.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Exercise(models.Model):
+    description = models.TextField() 
+    author = models.ForeignKey(User)
+
+    timestamp_last_updated = models.DateField(auto_now=True, auto_now_add=False)
+    timestamp_created = models.DateField(auto_now=False, auto_now_add=True)
+
+    #tells Django which field to use as display on the Django admin (analgous to console but on the browser)
+    def __str__(self):
+        return self.description
