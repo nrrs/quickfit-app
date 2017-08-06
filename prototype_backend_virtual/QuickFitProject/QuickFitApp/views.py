499fbb0f93e9ac946696424f816b92167df4d9d3
chrisbrickey <cbrickey1@gmail.com>
2017-08-06 03:50:19 -0700

    draft custom route to pull all exercises

--- prototype_backend_virtual/QuickFitProject/QuickFitApp/views.py
+++ prototype_backend_virtual/QuickFitProject/QuickFitApp/views.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Exercise
 
-# Create your views here.
+def index(request):
+    all_exercises = Exercise.objects.all()
+
+    # exercise_list = serializers.serialize('json', all_exercises)
+
+    exercise_dict = {}
+
+    for exercise in all_exercises:
+        exercise_dict = {'id' : exercise.id, 'name' : exercise.name, 'description' : exercise.description }
+
+    return JsonResponse({'exercises': exercise_dict})
--- prototype_backend_virtual/QuickFitProject/QuickFitProject/urls.py
+++ prototype_backend_virtual/QuickFitProject/QuickFitProject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from QuickFitApp.views import index
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^exercises/', index)
 ]
