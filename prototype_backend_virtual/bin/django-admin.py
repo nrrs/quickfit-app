b69d0133d7756da60ec0cc3484cf8c641a793afb
chrisbrickey <cbrickey1@gmail.com>
2017-08-06 16:03:01 -0700

    remove cached virtual environment files

--- prototype_backend_virtual/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/chrisbrickey/quickfit/prototype_backend/quickfit-app/prototype_backend_virtual/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
