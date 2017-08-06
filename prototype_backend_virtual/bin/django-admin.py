fb071511865d1e86ae17d70b87974efe97257050
chrisbrickey <cbrickey1@gmail.com>
2017-08-06 02:28:44 -0700

    create django project, ensure it runs on default server

---
+++ prototype_backend_virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/chrisbrickey/quickfit/prototype_backend/quickfit-app/prototype_backend_virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
