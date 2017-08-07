209ecf517867d7505547d75975d104a3b58ef78f
kevinshenyang07 <kevinshenyang07@gmail.com>
2017-08-07 11:43:51 -0700

    attempt to deploy to AWS

--- quickfitproject/quickfitapp/urls.py
+++ quickfitproject/quickfitapp/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^exercises/$', views.exercise_list),
-    url(r'^exercises/(?P<pk>[0-9]+)/$', views.exercise_detail),
+    url(r'^api/exercises/$', views.exercise_list),
+    url(r'^api/exercises/(?P<pk>[0-9]+)/$', views.exercise_detail),
 ]
