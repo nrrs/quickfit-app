bb5c6623b86396194133ce8542853a48119ec9a3
chrisbrickey <cbrickey1@gmail.com>
2017-08-07 19:31:13 -0700

    develop Movement model and migrate

--- quickfitproject/quickfitapp/models.py
+++ quickfitproject/quickfitapp/models.py
@@ -4,21 +4,30 @@ from django.db import models
 from django.contrib.auth.models import User
 
 MOVEMENT_TYPES = (
-    ('w', "Weight Lifting"),
-    ('d', "dance"),
+    ('b', "balance"),
     ('c', "cardio"),
-    ('o', "Others"),
+    ('d', "dance"),
+    ('f', "flexibility"),
+    ('o', "other"),
+    ('s', "strength"),
+    ('l', "stretch"),
+    ('w', "weights"),
+    ('y', "yoga")
 )
 
 
 class Movement(models.Model):
-    description = models.TextField()
-    author = models.ForeignKey(User)
+    author_id = models.ForeignKey(User)
+    title = models.CharField(max_length=100, blank=False)
+    description = models.TextField(blank=True)
+
     movement_type = models.CharField(
         choices = MOVEMENT_TYPES,
         max_length=1,
         default='o',
     )
+
+    demo_url = models.CharField(max_length=2000)
 
     timestamp_last_updated = models.DateField(auto_now=True, auto_now_add=False)
     timestamp_created = models.DateField(auto_now=False, auto_now_add=True)
