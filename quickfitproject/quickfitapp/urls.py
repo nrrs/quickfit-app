91cafea6c00db32d7daf1ddb614ba0126a9ff762
chrisbrickey <cbrickey1@gmail.com>
2017-08-08 09:08:00 -0700

    replace custom routes with automated routes using REST framework

--- quickfitproject/quickfitapp/urls.py
+++ quickfitproject/quickfitapp/urls.py
@@ -1,7 +1,12 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'movements', views.MovementViewSet)
+router.register(r'workouts', views.WorkoutViewSet)
 
 urlpatterns = [
-    url(r'^api/movements/$', views.movement_list),
-    url(r'^api/movements/(?P<pk>[0-9]+)/$', views.movement_detail),
+    url(r'^', include(router.urls)), #covers all routers defined above
+    # url(r'^movements/(?P<pk>[0-9]+)/$', views.movement_detail),
 ]
