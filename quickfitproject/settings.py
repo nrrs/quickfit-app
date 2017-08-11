b483076198f6a68060be24df9a50f261072f7094
chrisbrickey <cbrickey1@gmail.com>
2017-08-11 01:17:18 -0700

    adding static root

--- quickfitproject/settings.py
+++ quickfitproject/settings.py
@@ -172,9 +172,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+ASSETS_DIR = os.path.join(BASE_DIR, 'assets')
+
 STATICFILE_DIRS = [
-    os.path.join(BASE_DIR, 'assets'),
+    ASSETS_DIR,
 ]
+
+STATICFILE_ROOT = os.path.join(ASSETS_DIR, 'staticfiles')
 
 # WEBPACK_LOADER = {
 #   'DEFAULT': {
