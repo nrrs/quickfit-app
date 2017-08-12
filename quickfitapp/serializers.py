3619b968ef61202889e06ba7f2ed02b402b94d26
chrisbrickey <cbrickey1@gmail.com>
2017-08-11 20:52:21 -0700

    comment out duplicate User Serializer

--- quickfitapp/serializers.py
+++ quickfitapp/serializers.py
@@ -52,11 +52,11 @@ class ProfileSerializer(serializers.ModelSerializer):
 # from django.contrib.auth import get_user_model
 # User = get_user_model()
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id',
-                  'username')
+# class UserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ('id',
+#                   'username')
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
