3520415e6a65737a15c72fab834bfe61b58dccbd
chrisbrickey <cbrickey1@gmail.com>
2017-08-13 18:56:43 -0700

    turn off debugging for production, removed unnecessary allowed hosts

--- quickfitproject/settings.py
+++ quickfitproject/settings.py
@@ -5,13 +5,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'lw4++i2j_2u3^ejxk(t^@*+$0xza$r+ec8&6_v=#1p)52c8rd8'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.1.195',
-                 'localhost',
-                 '192.168.3.183',
-                 'afternoon-bastion-37946.herokuapp.com']
+ALLOWED_HOSTS = ['afternoon-bastion-37946.herokuapp.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
