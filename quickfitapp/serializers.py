3c4b30de5cb2f463978459a307dbf2755eef802b
chrisbrickey <cbrickey1@gmail.com>
2017-08-13 19:26:26 -0700

    remove comments on how to serialize data

--- quickfitapp/serializers.py
+++ quickfitapp/serializers.py
@@ -46,37 +46,3 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('id',
                   'email',
                   'username')
-
-
-
-# HOW TO USE ABOVE SERIALIZERS...
-# ModelSerializers are shorthand for regular serializers
-# they include default create() and update() methods when calling serializer.save()
-# print(repr(MovementSerializer)) to see the long hand form
-
-
-# to serialize data (transform an object instance into json)...
-
-# movementA = Movement(movement_name= 'abc')
-# movementA.save()
-# serializerA = MovementSerializer(movementA)
-# serializedA = serializerA.data ... '{"id": 2, "movement_name": u'abc', ...}' ...python native datatypes
-# contentA = JSONRenderer().render(serializedA) ... '{"id": 2, "movement_name": "abc", ...}'  ....json object
-
-
-# to deserialize data (transform a json object into an object instance) ...
-
-# from django.utils.six import BytesIO
-# streamB = BytesIO(contentA)
-# serializedB = JSONParser().parse(streamB)  ...at this point, data is python native
-# serializerB = MovementSerializer(serializedB=serializedB)
-# serializerB.validated_data
-# serializerB.save()
-
-
-# to serialize a querySet...
-
-# querySetC = Movement.objects.all()
-# serializerC = MovementSerializer(querySetC, many=True)
-# serializedC = serializerC.data ....python native datatypes
-# contentC = JSONRenderer().render(serializedC) ...json object
