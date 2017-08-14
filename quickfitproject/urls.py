9e027017a833b7e6bab96321dadca70b6eb0a413
chrisbrickey <cbrickey1@gmail.com>
2017-08-13 19:05:40 -0700

    cleanup project-level urls

--- quickfitproject/urls.py
+++ quickfitproject/urls.py
@@ -1,18 +1,3 @@
-"""quickfitproject URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url, include
 from django.views import static
 from django.conf import settings
@@ -21,14 +6,11 @@ from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    # url(r'^mobil-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
-
     url(r'^api/', include('quickfitapp.urls')),
     # url(r'^accounts/profile/', )  # calling api/users/login will redirect to this url
     url(r'^static/(?P<path>.*)$', static.serve, {
         'document_root': settings.STATIC_ROOT,
     }),
     url(r'^', views.landing_page),
-
 ]
